43fa95763dc8db913cdf8de570331a79dc1e6683
Mattheus Einloft <mattheuseinloft@gmail.com>
2021-07-15 18:55:25 -0300

    feat(users): add User repository

---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,17 @@
+import { getRepository, Repository } from 'typeorm';
+
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+}
+
+export default UsersRepository;
---
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -0,0 +1,5 @@
+import User from '../infra/typeorm/entities/User';
+import ICreateUserDTO from '../dtos/ICreateUserDTO';
+
+export default interface IUsersRepository {
+}
